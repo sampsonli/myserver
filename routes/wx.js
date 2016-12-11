c021fd1f83372a5be589613e8489b595d3442b59
sampsonli <740056710@qq.com>
2016-12-11 15:37:37 +0800

    fix bug

--- routes/wx.js
+++ routes/wx.js
@@ -13,7 +13,7 @@ router.all('/', wechat(config.token, function (req, res, next) {
     if(message.MsgType == 'text'){
         fetch(`https://op.juhe.cn/robot/index?key=dd3eaeb4eb005860d134561d933ff883&info=${encodeURI(message.Content)}`, {
             method: 'GET',
-        }).then(resp=>resp.text()).then(data=>{
+        }).then(resp=>resp.json()).then(data=>{
             if(data.error_code === 0){
                 res.reply({ type: "text", content: data.result.text});
             }
