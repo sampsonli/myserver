b084ec725996ee1d3ea4a273bc92c110f6ffa56c
sampsonli <740056710@qq.com>
2016-12-11 21:27:00 +0800

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    // First application
+    {
+      name      : "api",
+      script    : "./bin/www",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "lichun",
+      host : "138.128.207.90",
+      "key"  : "~/.ssh/id_rsa.pub",
+      ref  : "origin/master",
+      ssh_options: "StrictHostKeyChecking=no",
+      repo : "git@git.oschina.net:sampsonli/myserver.git",
+      path : "/lichun/myproj/myserver",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env api"
+    }
+  }
+}
