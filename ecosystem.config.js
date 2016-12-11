bc3d2c083da159457c209fc9faf220fc8418facf
sampsonli <740056710@qq.com>
2016-12-11 22:24:02 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
   deploy : {
     production : {
       user : "lichun",
-      host : "138.128.207.90",
+      host : "wxminapp.com",
       ref  : "origin/master",
       ssh_options: "StrictHostKeyChecking=no",
       repo : "git@git.oschina.net:sampsonli/myserver.git",
