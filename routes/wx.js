a8bbd8bb8b761fdef38c71604c34e0c8ed234325
sampsonli <740056710@qq.com>
2016-12-11 18:12:07 +0800

    fix bug

--- routes/wx.js
+++ routes/wx.js
@@ -10,39 +10,42 @@ let app = new WechatAPI(config.appid, config.appsecret);
 router.all('/', wechat(config.token, function (req, res, next) {
     // message is located in req.weixin
     var message = req.weixin;
-    if(message.MsgType == 'text'){
-        fetch(`https://op.juhe.cn/robot/index?key=dd3eaeb4eb005860d134561d933ff883&info=${encodeURI(message.Content)}&userid=${message.FromUser}`, {
-            method: 'GET',
-        }).then(resp=>resp.json()).then(data=>{
-            if(data.error_code === 0){
-                res.reply({ type: "text", content: data.result.text});
-            }else {
-                console.error(data.reason);
-                res.reply({ type: "text", content: '我爱你\ue056'});
-            }
-        }).catch(err=>{
-            console.error(err)
-        })
+    if (message.MsgType == 'text') {
 
-    }else if(message.MsgType === 'event'){
+        askQuestion(message.Content, message.FromUser, (msg)=>{
+            res.reply({type: "text", content: msg});
+        })
+    } else if (message.MsgType === 'event') {
         if (message.Event === 'subscribe') {
-            res.reply({ type: "text", content: '你好，请问有什么需要帮忙的吗？'});
+            res.reply({type: "text", content: '你好，请问有什么需要帮忙的吗？'});
         }
         if (message.Event === 'unsubscribe') {
-            res.reply({ type: "text", content: '感谢你的关注，欢迎下次再来。。。'});
+            res.reply({type: "text", content: '感谢你的关注，欢迎下次再来。。。'});
         }
+    } else if (message.MsgType === 'voice') {
+        askQuestion(message.Recognition, message.FromUser, (msg)=>{
+            res.reply({type: "text", content: msg});
+        })
     }
 }));
 
 
-fetch(`https://op.juhe.cn/robot/index?key=dd3eaeb4eb005860d134561d933ff883&info=${encodeURI('我')}`, {
-    method: 'GET',
-}).then(resp=>resp.json()).then(data=>{
-    console.log(data);
-}).catch(err=>{
-    console.error(err)
-})
-console.log(encodeURI('我'));
+function askQuestion(text, from, cb) {
+    fetch(`https://op.juhe.cn/robot/index?key=dd3eaeb4eb005860d134561d933ff883&info=${encodeURI(text)}&userid=${from}`, {
+        method: 'GET',
+    }).then(resp=>resp.json()).then(data=> {
+        if (data.error_code === 0) {
+            cb(data.result.text)
+        } else {
+            console.error(data.reason);
+            cb('我爱你\ue056')
+        }
+    }).catch(err=> {
+        console.error(err);
+        cb('我现在很忙，稍后再叫我')
+
+    })
+}
 
 
 module.exports = router;
