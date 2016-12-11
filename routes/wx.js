21e6cb4c9481930279eedb6089ad772f80e113f1
sampsonli <740056710@qq.com>
2016-12-11 15:55:15 +0800

    fix bug

--- routes/wx.js
+++ routes/wx.js
@@ -11,14 +11,13 @@ router.all('/', wechat(config.token, function (req, res, next) {
     // message is located in req.weixin
     var message = req.weixin;
     if(message.MsgType == 'text'){
-        console.log(encodeURI(message.Content));
         fetch(`https://op.juhe.cn/robot/index?key=dd3eaeb4eb005860d134561d933ff883&info=${encodeURI(message.Content)}`, {
             method: 'GET',
         }).then(resp=>resp.json()).then(data=>{
             if(data.error_code === 0){
                 res.reply({ type: "text", content: data.result.text});
             }else {
-                console.log(data)
+                res.reply({ type: "text", content: data.reason});
             }
         }).catch(err=>{
             console.error(err)
@@ -26,6 +25,8 @@ router.all('/', wechat(config.token, function (req, res, next) {
 
     }
 }));
+
+console.log(encodeURI('我'));
 
 
 module.exports = router;
