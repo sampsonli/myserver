de4f1ab78a6948de850dfb5cace5a0a9c1febb17
sampsonli <740056710@qq.com>
2016-12-11 15:32:02 +0800

    fix bug

--- config/index.js
+++ config/index.js
@@ -4,5 +4,6 @@
 module.exports = {
     appid: 'wx39c3ea2cb41a9169',
     appsecret: '476903c22b111a8034d13d31582adc74',
-    token: 'spring'
-}
+    token: 'spring',
+    encodingAESKey: '12dgGGR48yDTjvluf3sqQyqIlva2TsIIUlpYrfugBn1'
+};
--- routes/wx.js
+++ routes/wx.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var wechat = require('wechat');
+let fetch = require('node-fetch');
 var WechatAPI = require('wechat-api');
 let config = require('../config');
 let app = new WechatAPI(config.appid, config.appsecret);
@@ -10,7 +11,14 @@ router.all('/', wechat(config.token, function (req, res, next) {
     // message is located in req.weixin
     var message = req.weixin;
     if(message.MsgType == 'text'){
-        res.reply({ type: "text", content: "you input " + message.Content});
+        fetch(`https://op.juhe.cn/robot/index?key=dd3eaeb4eb005860d134561d933ff883&info=${encodeURI(text)}`, {
+            method: 'GET',
+        }).then(resp=>resp.text()).then(data=>{
+            if(data.error_code === 0){
+                res.reply({ type: "text", content: data.result.text});
+            }
+        })
+
     }
 }));
 
