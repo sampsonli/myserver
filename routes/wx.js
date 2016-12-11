1753c6b52e58e4fca361bcde43b689ffa737d9dc
sampsonli <740056710@qq.com>
2016-12-11 16:05:45 +0800

    add new feature

--- routes/wx.js
+++ routes/wx.js
@@ -11,13 +11,14 @@ router.all('/', wechat(config.token, function (req, res, next) {
     // message is located in req.weixin
     var message = req.weixin;
     if(message.MsgType == 'text'){
-        fetch(`https://op.juhe.cn/robot/index?key=dd3eaeb4eb005860d134561d933ff883&info=${encodeURI(message.Content)}`, {
+        fetch(`https://op.juhe.cn/robot/index?key=dd3eaeb4eb005860d134561d933ff883&info=${encodeURI(message.Content)}&userid=${message.FromUser}`, {
             method: 'GET',
         }).then(resp=>resp.json()).then(data=>{
             if(data.error_code === 0){
                 res.reply({ type: "text", content: data.result.text});
             }else {
-                res.reply({ type: "text", content: data.reason});
+                console.error(data.reason);
+                res.reply({ type: "text", content: '我爱你， 我都不好说什么好。。。'});
             }
         }).catch(err=>{
             console.error(err)
@@ -26,6 +27,14 @@ router.all('/', wechat(config.token, function (req, res, next) {
     }
 }));
 
+
+fetch(`https://op.juhe.cn/robot/index?key=dd3eaeb4eb005860d134561d933ff883&info=${encodeURI('我')}`, {
+    method: 'GET',
+}).then(resp=>resp.json()).then(data=>{
+    console.log(data);
+}).catch(err=>{
+    console.error(err)
+})
 console.log(encodeURI('我'));
 
 
