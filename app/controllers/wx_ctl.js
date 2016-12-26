e86f1ee57842403e22c28b93dce45c37e4ff8477
lichun <lichun@500wan.com>
2016-12-26 15:02:33 +0800

    fix bug

--- app/controllers/wx_ctl.js
+++ app/controllers/wx_ctl.js
@@ -7,7 +7,7 @@ import config from '../config'
 let app = new WechatAPI(config.appid, config.appsecret);
 
 
-export default wechat(config.token, function (req, res, next){
+export const all = wechat(config.token, function (req, res, next){
     // message is located in req.weixin
     var message = req.weixin;
     if (message.MsgType == 'text') {
