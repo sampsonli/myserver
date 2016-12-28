94859909f724826fb8f07d94e41ed083ca3464fb
sampsonli <740056710@qq.com>
2016-12-28 21:06:51 +0800

    add session

--- service.js
+++ service.js
@@ -5,10 +5,14 @@ import favicon from 'serve-favicon'
 import logger from 'morgan'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
+import session from 'express-session'
+import mongo from 'connect-mongo'
+import mongoose from 'mongoose'
 
 import './app/config/db'
 import routes from './app/routes'
 
+const MongoStore = mongo(session);
 const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +23,15 @@ app.use(favicon(path.join(__dirname, 'public', 'spring.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
+app.use(session({
+    secret: 'hello world',
+    name: 'sid',
+    cookie: {
+      // path: '.wxminapp.com'
+    },
+    store: new MongoStore({ mongooseConnection: mongoose.connection})
+}));
 app.use(express.static(path.join(__dirname, 'public'),{
   maxAge: '1h'
 }));
